c56d36b3a0c2240d826bd723843e35b671d53557
Luiz Gustavo <123706495+gustavococoo@users.noreply.github.com>
2023-10-18 14:24:26 -0300

    Add files via upload

---
+++ login/bd.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM acessar.users
+LIMIT 0, 1000
+
+-- Date: 2023-10-18 14:22
+*/
+INSERT INTO `` (`id`,`name`,`email`,`password`,`reset_token`) VALUES (1,'teste','teste@teste.com','$2y$10$GbK31ayKIxx539z59Jneo.FZlKdA7lhiy3q.rSdBtsbNVakMTKrP.','94cf5e2b981061256b419c867b39382bb7fd414e2d4519647f2764213181084d');
+INSERT INTO `` (`id`,`name`,`email`,`password`,`reset_token`) VALUES (3,'Luiz','luiz.carvalho@cest.edu.br','$2y$10$ABtNPxLh.uaQGFq6kKq1WewABQlcI5ro5bxrQTVvKd08T1MdEVCuG','ab8b9c18232deb5bd3f343018f9efa08c0bb6e141a24125be46b5ab2fb276158');
